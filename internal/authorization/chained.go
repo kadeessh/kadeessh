1c3b91c62060210c745cea61c35ec97622fa86c2
Mohammed Al Sahaf <msaa1990@gmail.com>
2022-03-11 18:30:30 +0300

    authorization: introduce Chained authorizer (#4)

---
+++ internal/authorization/chained.go
@@ -0,0 +1,119 @@
+package authorization
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/mohammed90/caddy-ssh/internal/session"
+	"github.com/mohammed90/caddy-ssh/internal/ssh"
+	"go.uber.org/multierr"
+	"go.uber.org/zap"
+)
+
+func init() {
+	caddy.RegisterModule(new(Chained))
+}
+
+type deauthor struct {
+	deauthor   DeauthorizeFunc
+	authorizer string
+}
+
+// Chained is a multi-authorizer module that authorizes a session against multiple authorizers
+type Chained struct {
+	AuthorizersRaw []json.RawMessage `json:"authorize,omitempty" caddy:"namespace=ssh.session.authorize inline_key=authorizer"`
+	authorizers    []Authorizer
+	logger         *zap.Logger
+}
+
+// This method indicates that the type is a Caddy
+// module. The returned ModuleInfo must have both
+// a name and a constructor function. This method
+// must not have any side-effects.
+func (c *Chained) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID: "ssh.session.authorize.chained",
+		New: func() caddy.Module {
+			return new(Chained)
+		},
+	}
+}
+
+// Provision loads up the sub-authorizers in the chain and provisions them as loaded
+func (c *Chained) Provision(ctx caddy.Context) error {
+	c.logger = ctx.Logger(c)
+	authzIface, err := ctx.LoadModule(c, "AuthorizersRaw")
+	if err != nil {
+		return fmt.Errorf("loading authorizer modules: %v", err)
+	}
+	for _, authorizer := range authzIface.([]interface{}) {
+		c.authorizers = append(c.authorizers, authorizer.(Authorizer))
+	}
+	return nil
+}
+
+// Authorize loops through the sub-authorizers in sequence asking for authorization and collects
+// the de-authorizers in a stack. If an authorization fails, it will de-authorize the earlier pushed
+// de-authorizers, except for the failed authorizer.
+func (c *Chained) Authorize(sess session.Session) (DeauthorizeFunc, bool, error) {
+	deauthors := []deauthor{}
+	authed := true
+	var err error
+	for i := 0; authed && i < len(c.authorizers); i++ {
+		deauth, a, autherr := c.authorizers[i].Authorize(sess)
+		authed = (authed && a) || autherr != nil
+		if autherr != nil {
+			c.logger.Error("error authorizing session",
+				zap.String("authorizer", fmt.Sprintf("%T", c.authorizers[i])),
+				zap.String("user", sess.User()),
+				zap.String("remote_ip", sess.RemoteAddr().String()),
+				zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+				zap.Error(autherr),
+			)
+			err = multierr.Append(err, nil)
+			break
+		}
+		// prepend
+		deauthors = append([]deauthor{{deauthor: deauth, authorizer: fmt.Sprintf("%T", c.authorizers[i])}}, deauthors...)
+	}
+	if !authed {
+		for _, deauther := range deauthors {
+			if perr := deauther.deauthor(sess); perr != nil {
+				c.logger.Error("error deauthorizing post failed authorization",
+					zap.String("authorizer", deauther.authorizer),
+					zap.String("user", sess.User()),
+					zap.String("remote_ip", sess.RemoteAddr().String()),
+					zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+					zap.Error(perr),
+				)
+				err = multierr.Append(err, perr)
+			}
+		}
+		return nil, authed, err
+	}
+	c.logger.Info("session authorized",
+		zap.String("user", sess.User()),
+		zap.String("remote_ip", sess.RemoteAddr().String()),
+		zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+	)
+	return func(s session.Session) error {
+		var err error
+		for _, deauther := range deauthors {
+			if perr := deauther.deauthor(s); perr != nil {
+				c.logger.Error("error deauthorizing session",
+					zap.String("authorizer", deauther.authorizer),
+					zap.String("user", sess.User()),
+					zap.String("remote_ip", sess.RemoteAddr().String()),
+					zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+					zap.Error(perr),
+				)
+				err = multierr.Append(err, perr)
+			}
+		}
+		return err
+	}, true, nil
+}
+
+var _ caddy.Provisioner = (*Chained)(nil)
+var _ Authorizer = (*Chained)(nil)
---
+++ internal/authorization/chained_test.go
@@ -0,0 +1,167 @@
+package authorization
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/mohammed90/caddy-ssh/internal/session"
+	"github.com/mohammed90/caddy-ssh/internal/ssh"
+	"go.uber.org/zap"
+)
+
+type dummyContext struct {
+	context context.Context
+	ssh.Session
+}
+
+func (dc dummyContext) Context() context.Context {
+	if dc.context == nil {
+		dc.context = context.WithValue(context.Background(), ssh.ContextKeySessionID, "session-id")
+	}
+	return dc.context
+}
+func (dc dummyContext) User() string {
+	return ""
+}
+func (dc dummyContext) RemoteAddr() net.Addr {
+	return &net.IPAddr{}
+}
+
+type dummyAuthorizer struct {
+	tag     string
+	success bool
+	err     error
+	writer  io.Writer
+}
+
+func (da *dummyAuthorizer) Authorize(s session.Session) (DeauthorizeFunc, bool, error) {
+	if da.err != nil {
+		return nil, da.success, da.err
+	}
+	fmt.Fprintf(da.writer, "Auth: %s\n", da.tag)
+	return func(session.Session) error {
+		fmt.Fprintf(da.writer, "De-Auth: %s\n", da.tag)
+		return nil
+	}, da.success, da.err
+}
+
+func TestChainedAuthorize(t *testing.T) {
+	outputBuffer := &bytes.Buffer{}
+	type fields struct {
+		authorizers []Authorizer
+	}
+	type args struct {
+		sess session.Session
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "",
+			fields: fields{
+				authorizers: []Authorizer{
+					&dummyAuthorizer{
+						tag:     "A",
+						success: true,
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "B",
+						success: true,
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "C",
+						success: true,
+						writer:  outputBuffer,
+					},
+				},
+			},
+			args: args{
+				sess: dummyContext{},
+			},
+			want: `Auth: A
+Auth: B
+Auth: C
+`,
+		},
+		{
+			name: "",
+			fields: fields{
+				authorizers: []Authorizer{
+					&dummyAuthorizer{
+						tag:     "A",
+						success: true,
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "B",
+						success: false,
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "C",
+						success: true,
+						writer:  outputBuffer,
+					},
+				},
+			},
+			args: args{
+				sess: dummyContext{},
+			},
+			want: `Auth: A
+Auth: B
+De-Auth: B
+De-Auth: A
+`,
+		},
+		{
+			name: "",
+			fields: fields{
+				authorizers: []Authorizer{
+					&dummyAuthorizer{
+						tag:     "A",
+						success: false,
+						err:     fmt.Errorf("error authorizing A"),
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "B",
+						success: true,
+						writer:  outputBuffer,
+					},
+					&dummyAuthorizer{
+						tag:     "C",
+						success: true,
+						writer:  outputBuffer,
+					},
+				},
+			},
+			args: args{
+				sess: dummyContext{},
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Chained{
+				authorizers: tt.fields.authorizers,
+				logger:      zap.NewNop(),
+			}
+			_, _, _ = c.Authorize(tt.args.sess)
+			got := outputBuffer.String()
+			if got != tt.want {
+				t.Errorf("Chained.Authorize() got = %v, want %v", got, tt.want)
+			}
+			outputBuffer.Reset()
+		})
+	}
+}

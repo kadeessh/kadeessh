9a4f6851c64c962f32cf0e8c9dcde03da19ea1d0
Mohammed Al Sahaf <msaa1990@gmail.com>
2022-03-11 18:24:23 +0300

    authorization: `Chained` authorizer godoc

--- internal/authorization/chained.go
+++ internal/authorization/chained.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/mohammed90/caddy-ssh/internal/session"
+	"github.com/mohammed90/caddy-ssh/internal/ssh"
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
 )
@@ -14,12 +15,16 @@ func init() {
 	caddy.RegisterModule(new(Chained))
 }
 
+type deauthor struct {
+	deauthor   DeauthorizeFunc
+	authorizer string
+}
+
+// Chained is a multi-authorizer module that authorizes a session against multiple authorizers
 type Chained struct {
 	AuthorizersRaw []json.RawMessage `json:"authorize,omitempty" caddy:"namespace=ssh.session.authorize inline_key=authorizer"`
-
-	authorizers []Authorizer
-
-	logger *zap.Logger
+	authorizers    []Authorizer
+	logger         *zap.Logger
 }
 
 // This method indicates that the type is a Caddy
@@ -35,9 +40,9 @@ func (c *Chained) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
+// Provision loads up the sub-authorizers in the chain and provisions them as loaded
 func (c *Chained) Provision(ctx caddy.Context) error {
 	c.logger = ctx.Logger(c)
-
 	authzIface, err := ctx.LoadModule(c, "AuthorizersRaw")
 	if err != nil {
 		return fmt.Errorf("loading authorizer modules: %v", err)
@@ -48,33 +53,61 @@ func (c *Chained) Provision(ctx caddy.Context) error {
 	return nil
 }
 
+// Authorize loops through the sub-authorizers in sequence asking for authorization and collects
+// the de-authorizers in a stack. If an authorization fails, it will de-authorize the earlier pushed
+// de-authorizers, except for the failed authorizer.
 func (c *Chained) Authorize(sess session.Session) (DeauthorizeFunc, bool, error) {
-	deauthors := []DeauthorizeFunc{}
+	deauthors := []deauthor{}
 	authed := true
 	var err error
-	for i := 0; authed && i < len(c.authorizers) && err == nil; i++ {
+	for i := 0; authed && i < len(c.authorizers); i++ {
 		deauth, a, autherr := c.authorizers[i].Authorize(sess)
 		authed = (authed && a) || autherr != nil
 		if autherr != nil {
-			err = autherr
+			c.logger.Error("error authorizing session",
+				zap.String("authorizer", fmt.Sprintf("%T", c.authorizers[i])),
+				zap.String("user", sess.User()),
+				zap.String("remote_ip", sess.RemoteAddr().String()),
+				zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+				zap.Error(autherr),
+			)
+			err = multierr.Append(err, nil)
+			break
 		}
 		// prepend
-		deauthors = append([]DeauthorizeFunc{deauth}, deauthors...)
+		deauthors = append([]deauthor{{deauthor: deauth, authorizer: fmt.Sprintf("%T", c.authorizers[i])}}, deauthors...)
 	}
 	if !authed {
-		var err error
 		for _, deauther := range deauthors {
-			if perr := deauther(sess); perr != nil {
+			if perr := deauther.deauthor(sess); perr != nil {
+				c.logger.Error("error deauthorizing post failed authorization",
+					zap.String("authorizer", deauther.authorizer),
+					zap.String("user", sess.User()),
+					zap.String("remote_ip", sess.RemoteAddr().String()),
+					zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+					zap.Error(perr),
+				)
 				err = multierr.Append(err, perr)
 			}
 		}
 		return nil, authed, err
 	}
-
+	c.logger.Info("session authorized",
+		zap.String("user", sess.User()),
+		zap.String("remote_ip", sess.RemoteAddr().String()),
+		zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+	)
 	return func(s session.Session) error {
 		var err error
 		for _, deauther := range deauthors {
-			if perr := deauther(s); perr != nil {
+			if perr := deauther.deauthor(s); perr != nil {
+				c.logger.Error("error deauthorizing session",
+					zap.String("authorizer", deauther.authorizer),
+					zap.String("user", sess.User()),
+					zap.String("remote_ip", sess.RemoteAddr().String()),
+					zap.String("session_id", sess.Context().Value(ssh.ContextKeySessionID).(string)),
+					zap.Error(perr),
+				)
 				err = multierr.Append(err, perr)
 			}
 		}

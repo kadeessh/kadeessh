9cb277cede2de0af779fbe2cce851d9799681639
Mohammed Al Sahaf <msaa1990@gmail.com>
2022-03-28 16:43:12 +0300

    chore: fix release script

--- .github/scripts/build-env.sh
+++ .github/scripts/build-env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-XCADDY_SKIP_BUILD=1 XCADDY_SKIP_CLEANUP=1 xcaddy build --with github.com/mohammed90/caddy-ssh
-mv buildenv_* custom-build
+XCADDY_SKIP_BUILD=1 XCADDY_SKIP_CLEANUP=1 $(go env GOPATH)/bin/xcaddy build --with github.com/mohammed90/caddy-ssh
+mv /tmp/buildenv_* custom-build

608ec9838ea6204cb2d52c98b70fbf61c9ce077d
Mohammed Al Sahaf <msaa1990@gmail.com>
2023-12-01 18:10:33 +0300

    debug: add more logging

--- internal/authentication/os/pubkey.go
+++ internal/authentication/os/pubkey.go
@@ -50,25 +50,53 @@ func (o *PublicKey) Provision(ctx caddy.Context) error {
 // if none of the list of keys in `authorized_keys` match the submitted keys.
 func (o *PublicKey) AuthenticateUser(ctx session.ConnMetadata, pubkey gossh.PublicKey) (authentication.User, bool, error) {
 	username := ctx.User()
+	o.logger.Debug(
+		"authenticating user",
+		zap.String("username", username),
+		zap.String("submitted_key_type", pubkey.Type()),
+	)
 	u, err := user.Lookup(username)
 	if err != nil {
 		return account{}, false, err
 	}
-	o.logger.Debug("authenticating user", zap.String("username", username))
 	authKeysFiles := filepath.Join(u.HomeDir, ".ssh", "authorized_keys")
+	o.logger.Debug(
+		"assumed path to authorized keys",
+		zap.String("user_homedir", u.HomeDir),
+		zap.String("path", authKeysFiles),
+	)
 	if _, err := os.Stat(authKeysFiles); err != nil && os.IsNotExist(err) {
 		return account{}, false, nil
+	} else if err != nil {
+		return account{}, false, err
 	}
 	authKeysBytes, err := os.ReadFile(authKeysFiles)
 	if err != nil {
 		return account{}, false, err
 	}
+	o.logger.Debug(
+		"loaded authorized keys",
+		zap.String("path", authKeysFiles),
+		zap.Int("size", len(authKeysBytes)),
+	)
+	keysCount := 0
 	for len(authKeysBytes) > 0 {
-		key, _, _, rest, err := ssh.ParseAuthorizedKey(authKeysBytes)
+		key, comment, options, rest, err := ssh.ParseAuthorizedKey(authKeysBytes)
 		if err != nil {
 			return account{}, false, err
 		}
+		keysCount++
+		o.logger.Debug(
+			"parsed an authorized key",
+			zap.String("key_type", key.Type()),
+			zap.String("comment", comment),
+			zap.Strings("options", options),
+		)
 		if ssh.KeysEqual(key, pubkey) {
+			o.logger.Debug(
+				"authorized key matched",
+				zap.String("key_type", key.Type()),
+			)
 			return account{
 				user: u,
 				permissions: &gossh.Permissions{
@@ -85,5 +113,6 @@ func (o *PublicKey) AuthenticateUser(ctx session.ConnMetadata, pubkey gossh.Publ
 		}
 		authKeysBytes = rest
 	}
+	o.logger.Debug("loaded authorized keys", zap.Int("count", keysCount))
 	return account{}, false, nil // TODO: report an error?
 }
